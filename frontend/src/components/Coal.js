e4784808d7d2b2c3f64af8e7ecd4928c520e5a1e
hukum <151901791+rogerkahukum@users.noreply.github.com>
2024-03-20 11:48:38 +0530

    Login notification (Succesesful login noti is shown 2 times)

--- frontend/src/components/Coal.js
+++ frontend/src/components/Coal.js
@@ -4,6 +4,10 @@ import {
   ShoppingOutlined,
   UserOutlined,
 } from "@ant-design/icons";
+import { Button, Divider, notification } from "antd";
+import React from "react";
+import { useMemo } from "react";
+
 import { Card, Space, Statistic } from "antd";
 import { useEffect, useState } from "react";
 import { getCustomers, getInventory, getOrders } from "../API";
@@ -19,7 +23,7 @@ import {
   Legend,
 } from "chart.js";
 import "./coal.css";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 ChartJS.register(
   CategoryScale,
@@ -29,8 +33,26 @@ ChartJS.register(
   Tooltip,
   Legend
 );
-
+const Context = React.createContext({
+  name: "Default",
+});
 function Dashboard() {
+  const [api, contextHolder] = notification.useNotification();
+  const openNotification = (placement) => {
+    api.success({
+      message: `Login Successful !`,
+
+      placement,
+      duration: 2,
+    });
+  };
+  const contextValue = useMemo(
+    () => ({
+      name: "Ant Design",
+    }),
+    []
+  );
+  // openNotification("bottom");
   const navigate = useNavigate();
 
   const logout = () => {
@@ -38,16 +60,28 @@ function Dashboard() {
     localStorage.removeItem("token");
     localStorage.removeItem("id");
     localStorage.removeItem("OrganizationName");
-    
-    navigate("/login");
 
-};
+    navigate("/login");
+  };
   const [orders, setOrders] = useState(0);
   const [inventory, setInventory] = useState(0);
   const [customers, setCustomers] = useState(0);
   const [revenue, setRevenue] = useState(0);
+  const [isNotified, setIsNotified] = useState(0);
+  const notify = (v) => {
+    if (!v) {
+      console.log("logged in");
+      console.log(v);
 
+      openNotification("bottom");
+      setIsNotified(1);
+    }
+  };
   useEffect(() => {
+    // notify(isNotified);
+
+    //useEffect is ran twice with the initial state of 0.(An issue)
+    notify(isNotified);
     getOrders().then((res) => {
       setOrders(res.total);
       setRevenue(res.discountedTotal);
@@ -61,87 +95,90 @@ function Dashboard() {
   }, []);
   const OrganizationName = localStorage.getItem("OrganizationName");
   return (
-    <div>
-      <div className="coal" id="coal">
-        <div>
-          <Space size={20} direction="vertical">
-            <h2>{OrganizationName}</h2>
-            <h4>DashBoard</h4>
-            <Space direction="horizontal">
-              <DashboardCard
-                icon={
-                  <ShoppingCartOutlined
-                    style={{
-                      color: "green",
-                      backgroundColor: "rgba(0,255,0,0.25)",
-                      borderRadius: 20,
-                      fontSize: 24,
-                      padding: 8,
-                    }}
-                  />
-                }
-                title={"Stocks"}
-                value={orders}
-              />
-              <DashboardCard
-                icon={
-                  <ShoppingOutlined
-                    style={{
-                      color: "blue",
-                      backgroundColor: "rgba(0,0,255,0.25)",
-                      borderRadius: 20,
-                      fontSize: 24,
-                      padding: 8,
-                    }}
-                  />
-                }
-                title={"Orders Recived"}
-                value={inventory}
-              />
-              <DashboardCard
-                icon={
-                  <UserOutlined
-                    style={{
-                      color: "purple",
-                      backgroundColor: "rgba(0,255,255,0.25)",
-                      borderRadius: 20,
-                      fontSize: 24,
-                      padding: 8,
-                    }}
-                  />
-                }
-                title={"Active Users"}
-                value={customers}
-              />
-              <DashboardCard
-                icon={
-                  <DollarCircleOutlined
-                    style={{
-                      color: "red",
-                      backgroundColor: "rgba(255,0,0,0.25)",
-                      borderRadius: 20,
-                      fontSize: 24,
-                      padding: 8,
-                    }}
-                  />
-                }
-                title={"Total Sales"}
-                value={revenue}
-              />
+    <Context.Provider value={contextValue}>
+      {contextHolder}
+      <div>
+        <div className="coal" id="coal">
+          <div>
+            <Space size={20} direction="vertical">
+              <h2>{OrganizationName}</h2>
+              <h4>DashBoard</h4>
+              <Space direction="horizontal">
+                <DashboardCard
+                  icon={
+                    <ShoppingCartOutlined
+                      style={{
+                        color: "green",
+                        backgroundColor: "rgba(0,255,0,0.25)",
+                        borderRadius: 20,
+                        fontSize: 24,
+                        padding: 8,
+                      }}
+                    />
+                  }
+                  title={"Stocks"}
+                  value={orders}
+                />
+                <DashboardCard
+                  icon={
+                    <ShoppingOutlined
+                      style={{
+                        color: "blue",
+                        backgroundColor: "rgba(0,0,255,0.25)",
+                        borderRadius: 20,
+                        fontSize: 24,
+                        padding: 8,
+                      }}
+                    />
+                  }
+                  title={"Orders Recived"}
+                  value={inventory}
+                />
+                <DashboardCard
+                  icon={
+                    <UserOutlined
+                      style={{
+                        color: "purple",
+                        backgroundColor: "rgba(0,255,255,0.25)",
+                        borderRadius: 20,
+                        fontSize: 24,
+                        padding: 8,
+                      }}
+                    />
+                  }
+                  title={"Active Users"}
+                  value={customers}
+                />
+                <DashboardCard
+                  icon={
+                    <DollarCircleOutlined
+                      style={{
+                        color: "red",
+                        backgroundColor: "rgba(255,0,0,0.25)",
+                        borderRadius: 20,
+                        fontSize: 24,
+                        padding: 8,
+                      }}
+                    />
+                  }
+                  title={"Total Sales"}
+                  value={revenue}
+                />
+              </Space>
+              <Space>
+                <DashboardChart />
+              </Space>
             </Space>
-            <Space>
-              <DashboardChart />
-            </Space>
-          </Space>
-        </div>
-        <div>
-          <div className="logout">
-            <button onClick={logout}>Logout</button>
           </div>
-          <Chart />
+          <div>
+            <div className="logout">
+              <button onClick={logout}>Logout</button>
+            </div>
+            <Chart />
+          </div>
         </div>
       </div>
-    </div>
+    </Context.Provider>
   );
 }
 
@@ -159,8 +196,7 @@ function DashboardCard({ title, value, icon }) {
 function DashboardChart() {
   return (
     <div className="coal__next">
-
-        <Details />
+      <Details />
     </div>
   );
 }
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,19 +1,46 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import axios from "axios";
 import "./login.css";
 import img from "../img/login-coal.png";
+// const { Button, Divider, notification, Space } = antd;
+import { Button, Divider, notification, Space } from "antd";
+
+const Context = React.createContext({
+  name: "Default",
+});
 function LoginUser() {
+  const [api, contextHolder] = notification.useNotification();
+  const openNotification = (placement) => {
+    api.error({
+      message: `Invalid Credentials`,
+      description:
+        // (<Context.Consumer>{({ name }) => `Hello, ${name}!`}</Context.Consumer>)
+        `Please check the enter username and password.`,
+      placement,
+      duration: 2,
+    });
+  };
+  const contextValue = useMemo(
+    () => ({
+      name: "Ant Design",
+    }),
+    []
+  );
+  const showNotification = () => {
+    // console.log(error.response.status);
+    openNotification("bottom");
+  };
   const [data, setData] = useState({ userid: "", password: "" });
   // eslint-disable-next-line
-  const [error, setError] = useState(""); 
+  const [error, setError] = useState("");
   const handleChange = ({ currentTarget: input }) => {
     setData({ ...data, [input.placeholder]: input.value });
     console.log(input.type);
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
+    // openNotification("bottom");
     console.log(data);
 
     try {
@@ -26,6 +53,7 @@ function LoginUser() {
       localStorage.setItem("Type", res.Type);
       localStorage.setItem("OrganizationName", res.OrganizationName);
       localStorage.setItem("id", res.id);
+      // openNotification("top");
       window.location = "/";
     } catch (error) {
       if (
@@ -34,44 +62,60 @@ function LoginUser() {
         error.response.status <= 500
       ) {
         setError(error.response.data.message);
-        console.log(error);
+        console.log(error.response.status);
+        if (error.response.status === 401) {
+          openNotification("bottom");
+          console.log("Incorrect credentials.");
+        }
       }
     }
   };
   return (
-    <div className="login__app">
-      <div className="login-wrapper">
-        {/* <h1 className="login">LOG IN </h1> */}
-        <form onSubmit={handleSubmit}>
-          <label>
-            <p>UserID</p>
-            <input
-              type="text"
-              value={data.userid}
-              onChange={handleChange}
-              placeholder="userid"
-              required="required"
-            />{" "}
-          </label>
-          <label>
-            <p className="label">Password</p>
-            <input
-              type="password"
-              onChange={handleChange}
-              value={data.password}
-              placeholder="password"
-              required="required"
-            />
-          </label>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
+    <Context.Provider value={contextValue}>
+      {contextHolder}
+      <div className="login__app">
+        <div className="login-wrapper">
+          {/* <h1 className="login">LOG IN </h1> */}
+          <form onSubmit={handleSubmit}>
+            <label>
+              <p>UserID</p>
+              <input
+                type="text"
+                value={data.userid}
+                onChange={handleChange}
+                placeholder="userid"
+                required="required"
+              />{" "}
+            </label>
+            <label>
+              <p className="label">Password</p>
+              <input
+                type="password"
+                onChange={handleChange}
+                value={data.password}
+                placeholder="password"
+                required="required"
+              />
+            </label>
+            <div>
+              <button
+                type="submit"
+                onClick={() => {
+                  console.log("submit clicked");
+                  // showNotification();
+                  // openNotification("bottom");
+                }}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+        <div>
+          <img src={img} alt="pic" />
+        </div>
       </div>
-      <div>
-        <img src={img} alt="pic" />
-      </div>
-    </div>
+    </Context.Provider>
   );
 }
 
